9cc8881c6032fb369f52a4f3c5245b20d08fdd63
Gracie Parce <graceehparce@gmail.com>
2022-11-11 13:03:17 -0600

    updated posts and rare_users

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -1,3 +1,4 @@
 from .category import Category
 from .posts import Post
 from .comment import Comment
+from .rare_user import Rare_User
---
+++ rareapi/views/rare_user_view.py
@@ -0,0 +1,48 @@
+from datetime import date
+from django.http import HttpResponseServerError
+from rest_framework import serializers, status
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rareapi.models import Post
+from rareapi.models import Rare_User
+
+
+class RareUserView(ViewSet):
+
+    def retrieve(self, request, pk):
+
+        rare_user = Rare_User.objects.get(pk=pk)
+        serializer = RareUserSerializer(rare_user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request):
+
+        rare_users = Rare_User.objects.all()
+        serializer = RareUserSerializer(rare_users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+
+        new_rare_user = Rare_User()
+        new_rare_user.user = request.auth.user
+        new_rare_user.active = request.data["active"]
+        new_rare_user.profile_image_url = request.data["profile_image_url"]
+        new_rare_user.created_on = date.today()
+        new_rare_user.bio = request.data["bio"]
+        new_rare_user.save()
+
+        serializer = RareUserSerializer(new_rare_user)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk):
+        rare_user = Rare_User.objects.get(pk=pk)
+        rare_user.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class RareUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'active', 'profile_image_url',
+                  'created_on', 'bio')
