4bd91a745e65806ca998135923208656cc6608ca
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-11-11 15:39:57 -0600

    Updating Rare User Field

--- rareapi/models/rare_user.py
+++ rareapi/models/rare_user.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Rare_User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    active = models.CharField(max_length=250)
+    active = models.BooleanField()
     profile_image_url = models.CharField(max_length=250)
     created_on = models.DateField()
     bio = models.CharField(max_length=300)
